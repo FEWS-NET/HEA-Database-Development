097af8ca48fbb14391a4f3eda2f12a3f7e6f39eb
Girum Bizuayehu <gbizuayehu@fews.net>
2023-08-17 05:48:56 +0000

    Remove bootstrap until deciding on JS/CSS approach see HEA-78

--- hea/urls.py
+++ hea/urls.py
@@ -3,15 +3,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from baseline.views import ModelDocStringsAPI
-
 router = routers.DefaultRouter()
 # router.register(r"livelihoodcategories", LivelihoodCategoryViewSet)
 
 urlpatterns = [
     ########## LOCALE INDEPENDENT PATHS go here. ##########
     path("api/", include(router.urls)),
-    path("api/model_doc_strings/<str:model_name>", ModelDocStringsAPI.as_view()),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ] + i18n_patterns(
     ########## LOCALE DEPENDENT PATHS go here. ##########
