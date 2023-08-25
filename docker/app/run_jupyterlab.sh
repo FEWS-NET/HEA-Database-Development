2465c5dcb70c3c4b8c790660c5fda5bc20dc3182
Roger Hunwicks <roger@tonic-solutions.com>
2023-08-24 22:37:16 -0400

    Configure jupyter without a user home directory - see HEA-109

--- docker/app/run_jupyterlab.sh
+++ docker/app/run_jupyterlab.sh
@@ -4,11 +4,11 @@ echo Compiling translations
 python manage.py compilemessages
 
 # Wait up to 30 minutes for outstanding migrations to finish running on another container, checking every 30 seconds
-#for i in `seq 1 60`; do
-#    ./manage.py migrate --plan | grep "No planned migration operations." && break
-#    echo Waiting for outstanding database migrations
-#    sleep 30
-#done
+for i in `seq 1 60`; do
+    ./manage.py migrate --plan | grep "No planned migration operations." && break
+    echo Waiting for outstanding database migrations
+    sleep 30
+done
 
 # Need to create and set permissions on the logs so that we can run as the django user and
 # still have permission to write to the logs directory using docker-compose.override.yml
@@ -16,6 +16,13 @@ echo Setting up logs
 touch log/django.log
 chown -R django:django log/*
 
-echo Starting JupyterLab in the /notebooks directory
-cd /usr/src/app/notebooks
-gosu django jupyter lab --notebook-dir=/usr/src/app/notebooks --preferred-dir=/usr/src/app/notebooks --port=8888 --no-browser
+# @TODO Move the pip install of Jupyter here, and remove it from requirements/base.txt, to reduce the number of
+# Python packages installed in the base image.
+# pip install jupyterlab==4.0.5
+
+echo Starting JupyterLab
+IPYTHONDIR=/usr/src/app/jupyter/ipython \
+JUPYTER_CONFIG_DIR=/usr/src/app/jupyter/lab/user-settings \
+JUPYTER_DATA_DIR=/usr/src/app/jupyter/jupyter/data \
+JUPYTER_RUNTIME_DIR=/usr/src/app/run/jupyter/ \
+gosu django jupyter lab --notebook-dir=/usr/src/app/notebooks --ip=0.0.0.0 --port=8888 --no-browser
