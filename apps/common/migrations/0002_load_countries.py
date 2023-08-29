d4f29a9a0b751876ec395f76bdb122265f3f8dbd
Girum Bizuayehu <gbizuayehu@fews.net>
2023-08-29 14:20:10 +0000

    Update migrations to avoid running them while running tests see HEA-78

--- apps/common/migrations/0002_load_countries.py
+++ apps/common/migrations/0002_load_countries.py
@@ -1,8 +1,20 @@
 import os.path
 
+from django.conf import settings
 from django.db import migrations, models
 
 from common.utils import LoadModelFromDict
+
+
+def forwards(apps, schema_editor):
+    # To avoid running migrations in tests
+    if "test" not in settings.DATABASES["default"]["NAME"]:
+        LoadModelFromDict(model="Country", data=os.path.splitext(os.path.abspath(__file__))[0] + ".txt"),
+
+
+def backwards(apps, schema_editor):
+    Country = apps.get_model("common", "Country")
+    Country.objects.all().delete()
 
 
 class Migration(migrations.Migration):
@@ -12,5 +24,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        LoadModelFromDict(model="Country", data=os.path.splitext(os.path.abspath(__file__))[0] + ".txt"),
+        migrations.RunPython(forwards, backwards),
     ]
