df99219f418c7254f7ffb4b5554c0991dd540e5c
Girum Bizuayehu <gbizuayehu@fews.net>
2023-09-05 23:13:59 +0000

    Update migrations to remove conditional migration that check test see HEA-111

--- apps/common/migrations/0002_load_countries.py
+++ apps/common/migrations/0002_load_countries.py
@@ -6,17 +6,6 @@ from django.db import migrations, models
 from common.utils import LoadModelFromDict
 
 
-def forwards(apps, schema_editor):
-    # To avoid running migrations in tests
-    if "test" not in settings.DATABASES["default"]["NAME"]:
-        LoadModelFromDict(model="Country", data=os.path.splitext(os.path.abspath(__file__))[0] + ".txt"),
-
-
-def backwards(apps, schema_editor):
-    Country = apps.get_model("common", "Country")
-    Country.objects.all().delete()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -24,5 +13,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(forwards, backwards),
+        LoadModelFromDict(model="Country", data=os.path.splitext(os.path.abspath(__file__))[0] + ".txt"),
     ]
