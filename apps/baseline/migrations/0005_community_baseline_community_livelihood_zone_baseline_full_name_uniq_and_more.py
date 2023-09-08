83bfd9e940fd0c87e2d18efe6fc1273465a5bd1b
Roger Hunwicks <roger@tonic-solutions.com>
2023-09-08 14:53:13 -0400

    Rename migration - see HEA-90

--- apps/baseline/migrations/0005_community_baseline_community_livelihood_zone_baseline_full_name_uniq_and_more.py
+++ apps/baseline/migrations/0005_community_baseline_community_livelihood_zone_baseline_full_name_uniq_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("baseline", "0003_add_community_full_name_and_code"),
+        ("baseline", "0004_add_wealth_group_characteristic_value_source_and_product"),
     ]
 
     operations = [
