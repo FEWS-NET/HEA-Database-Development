dd1990937c1807f973cc7ac23c62de4f7d4ca5ce
Roger Hunwicks <roger@tonic-solutions.com>
2024-02-09 22:24:08 -0500

    Minor verbose_json improvements - see HEA-98
    Better error logging when import data using the
    verbose_json format, to make it easier to
    trouble-shoot loading data through fixtures.

--- apps/common/management/commands/verbose_load_data.py
+++ apps/common/management/commands/verbose_load_data.py
@@ -1,7 +1,7 @@
-import json
 import logging
 
 from django.core.management.commands import loaddata
+from django.forms.models import model_to_dict
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +15,9 @@ class Command(loaddata.Command):
         try:
             return super().save_obj(obj)
         except Exception as e:
-            # @TODO handle unserializable objects
-            logger.exception(e, extra={"object": json.dumps(obj.object.__dict__)})
-            raise
+            # Reraise the error with a serialized representation of the object that cauase the exception.
+            # We can't use json.dumps because attributes such as datetime fields and the ModelState are not
+            # serializable. Use Django Form's `model_to_dict` function to avoid these issues.
+            raise RuntimeError(
+                "Failed to save %s %s" % (obj.object._meta.verbose_name, model_to_dict(obj.object))
+            ) from e
