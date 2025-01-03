f821c6fdbd0c3ef75a1f82928569edc0ce47f059
Eliud Njoki <enjoki@fews.net>
2025-01-03 14:32:06 +0300

    HEA-592 first version of Inventory Dashboard

--- apps/viz/urls.py
+++ apps/viz/urls.py
@@ -1,10 +1,11 @@
-
 from django.urls import path
+
+import viz.dash.inventory_dashboard.app
 
 from .views import InventoryDashboardView
 
 app_name = "viz"
 
 urlpatterns = [
-    path("", InventoryDashboardView.as_view(), name="Inventory_dashboard"),
+    path("", InventoryDashboardView.as_view(), name="inventory_dashboard"),
 ]
