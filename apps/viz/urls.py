7a575fb80d0782f3792d4d07c61fc232805d05f3
Girum Bizuayehu <girum.bizuayehu@gmail.com>
2025-09-24 15:58:46 +0300

    Remove unused module, apply linting see HEA-689

--- apps/viz/urls.py
+++ apps/viz/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 
 from viz.dash.inventory_dashboard import app as inventory  # NOQA: F401
 from viz.dash.pipeline_status_dashboard import app as pipeline_status  # NOQA: F401
-from viz.views import (
-    InventoryDashboardView,
-    PipelineStatusDashboardView,
-)
+from viz.views import InventoryDashboardView, PipelineStatusDashboardView
 
 app_name = "viz"
 
