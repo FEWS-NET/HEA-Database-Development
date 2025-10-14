466a6035e075e6f26738a7363fa9a1cb123e2be9
Girum Bizuayehu <girum.bizuayehu@gmail.com>
2025-10-14 11:34:02 +0300

    Override common app ready to patch urllib3 pool see HEA-752

--- apps/common/apps.py
+++ apps/common/apps.py
@@ -6,3 +6,22 @@ class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "common"
     verbose_name = _("common")
+
+    def ready(self):
+        """Patch urllib3 connection pool size on startup"""
+        from urllib3 import HTTPConnectionPool
+
+        original_init = HTTPConnectionPool.__init__
+
+        def patched_init(self, *args, **kwargs):
+            # Force larger pool size
+            kwargs["maxsize"] = kwargs.get("maxsize", 50)
+            if kwargs["maxsize"] < 50:
+                kwargs["maxsize"] = 50
+
+            original_init(self, *args, **kwargs)
+
+        # Apply patch
+        HTTPConnectionPool.__init__ = patched_init
+
+        print("urllib3 connection pool patched: maxsize=50")
