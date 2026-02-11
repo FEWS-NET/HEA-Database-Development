e10beb9c2df27965789fd2dfa423b30bc4aa71fc
Roger Hunwicks <rhunwicks@air.org>
2026-02-11 15:24:24 -0500

    Fix misnumbered migration for Season.purpose - see HEA-787

--- apps/metadata/migrations/0015_season_purpose.py
+++ apps/metadata/migrations/0015_season_purpose.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("metadata", "0013_wealthcharacteristic_characteristic_group"),
+        ("metadata", "0014_characteristicgroup_and_more"),
     ]
 
     operations = [
